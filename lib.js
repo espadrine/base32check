7115ea00b36502e32f8ec1fd8624a4bc5d47682c
Thaddee Tyl <thaddee.tyl@gmail.com>
2019-04-18 22:32:46 +0200

    Fix division in checksum computation
    Finite field division is not equivalent to integer division with
    modulo.
    
    We compute the inverse of the number to divide in O(1) by
    precomputing all powers of the primitive element of the finite
    field.

--- lib.js
+++ lib.js
@@ -15,20 +15,38 @@ function toBase32Char(c) {
   return String.fromCharCode(d);
 }
 
-function checksum(payload) {
-  let primitive = 3;
+const powersOf3 = [1, 3, 9, 27, 19, 26, 16, 17, 20, 29, 25, 13, 8, 24, 10,
+  30, 28, 22, 4, 12, 5, 15, 14, 11, 2, 6, 18, 23, 7, 21];
 
+function checksum(payload) {
+  const primitive = 3;
+  const n = payload.length;
+
+  // We must solve Σ ai P^i = 0 for i from 1 to n+1.
+  // First, compute Σ ai P^i for i from 1 to n.
   let sum = 0;
   let p = 1;
-  for (let i = 0; i < payload.length; i++) {
-    let c = fromBase32Char(payload[i]);
+  for (let i = 0; i < n; i++) {
+    const c = fromBase32Char(payload[i]);
+    // We could use powersOf3 here, although this may be faster.
     p = (p * primitive) % cardinal;
-    sum = (sum + c * p) % cardinal;
+    sum = (sum + c * (p % cardinal)) % cardinal;
+    console.log(`c ${c}\tp ${p}\tsum ${sum}`);
   }
 
-  let opposite = cardinal - sum;
-  p = (p * primitive) % cardinal;
-  let code = Math.floor(opposite / p);
+  // We must solve:  sum + code * primitive^(n+1) = 0
+  // That is:        sum + opposite           = 0
+  // Therefore:      opposite = -sum
+  const opposite = (sum === 0)? 0: (cardinal - sum);
+
+  // We must solve:  code * primitive^(n+1) = opposite
+  // We know:        a^(cardinal-1) = 1
+  // Therefore:      a^(cardinal-2) * a = 1
+  // Here we have:   a = primitive^(n+1)
+  // Hence:          code = opposite * primitive^((cardinal-2)*(n+1))
+  const inverse = powersOf3[((cardinal-2)*(n+1)) % (cardinal - 1)];
+  let code = (opposite * inverse) % cardinal;
+  console.log(`opposite ${opposite}\tinverse ${inverse}\tp ${p}\tcode ${code}`);
   return toBase32Char(code);
 }
 
